0e4f412faec21fa9d07a9c0777ed036f0d4682c6
nadeem <er.nadeemaslam89@gmail.com>
2015-08-17 12:39:05 +0530

    bugs fixed

--- access/tests.py
+++ access/tests.py
@@ -1,12 +1,17 @@
+import unittest
 from django.test import TestCase
 
 from access.api import UserInstanceResource
 from django.test.client import Client
+from django.core.urlresolvers import reverse
+from django.test.client import RequestFactory
+from access.views import LoginView
+from django.contrib.auth.models import User
 from django.test.utils import setup_test_environment
 setup_test_environment()
 
 
-class TestData(TestCase):
+class TestData(unittest.TestCase):
 	'''base class for testing'''
 	
 	def setUp(self):
@@ -28,9 +33,36 @@ class TestData(TestCase):
 		s=UserInstanceResource()._get(email='nadeem@gmail.com')
 		self.assertEqual(s.first_name,'farhan')
 
-	
+	# def test_call_view_loads(self):
+ #    	self.client.login(username='sa@sa.com', password='123')
+ #    	response = self.client.get('/super_home')
+ #    	self.assertEqual(response.status_code, 200)
+ #    	self.assertTemplateUsed(response, 'superadmin_dashboard.html')
 
 	
+ 	def tearDown(self):
+ 		self.test_user.delete()
+ 		
+	def test_login(self):
+	 	self.client = Client()
+	 	# self.first_name = 'agconti'
+	 	self.email = 'sa@sa.com'
+	 	self.password = '123'
+	 	# self.test_user = User.objects.create_user(self.email, self.password)
+	 	login = self.client.login(email=self.email, password=self.password)
+	 	self.assertEqual(login, False)
+
+
+	# def setUp(self):
+	# 	self.factory = RequestFactory()
+
+	# def test_list_view(self):
+	# 	request = self.factory.get(reverse('super_home'))
+ #        # additional params can go after request
+ #        response = LoginView.as_view()(request)
+ #        self.assertEqual(response.status_code, 200)
+
+
 
 
 
